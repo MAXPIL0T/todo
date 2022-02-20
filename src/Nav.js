d32ddd23eb6c0cfe686db12c870e44385a7323fd
Maximilian Kuechen <maximilian.kuechen@gmail.com>
2022-02-19 23:57:08 -0500

    Nav is now there without css

---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <div className="nav">
+            <p>My amazing todo list</p>
+        </div>
+    );
+
+}
+
+export default Nav;
---
+++ src/Todo.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+function Todo() {
+    
+    return (
+        <div className="todo">
+
+        </div>
+    );
+}
+
+export default Todo;
