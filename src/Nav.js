f8c30a7fb7b05b590dfe519294e9fe35cf15048b
Maximilian Kuechen <maximilian.kuechen@gmail.com>
2022-04-10 23:09:52 -0400

    Nico lol

--- src/Nav.js
+++ src/Nav.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
-import github from './img/github.png';
-import m from './img/m.svg';
+// import github from './img/github.png';
+// import m from './img/m.svg';
 
 function Nav() {
     return (
         <div className="Nav">
-            <a href="https://www.maxkuechen.com" target="_blank" rel="noopener noreferrer">
+            {/* <a href="https://www.maxkuechen.com" target="_blank" rel="noopener noreferrer">
                 <img src={m} alt="M" />
-            </a>
-            <p>todo.</p>
-            <a href="https://github.com/MAXPIL0T/todo" target="_blank" rel="noopener noreferrer">
+            </a> */}
+            <p>homework tracker.</p>
+            {/* <a href="https://github.com/MAXPIL0T/todo" target="_blank" rel="noopener noreferrer">
                 <img src={github} alt="GitHub" />
-            </a>
+            </a> */}
         </div>
     );
 }
